c9f65f504292a858756be7114552c43768ae0ef1
Mario González <mario@mariogl.com>
2022-03-10 23:26:36 +0100

    Added info from last commit in project card

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -6,6 +6,20 @@ import Project from "../../types/project";
 import RootState from "../../types/store";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
+const colors = [
+  "#d50158",
+  "#8900a6",
+  "#3330a3",
+  "#0084d6",
+  "#009baa",
+  "#007e6b",
+  "#009534",
+  "#abb901",
+  "#ffc100",
+  "#ff7101",
+  "#f92801",
+];
+
 const ProjectsList = (): JSX.Element => {
   const dispatch = useDispatch();
   const projects: Project[] = useSelector((state: RootState) => state.projects);
@@ -16,9 +30,17 @@ const ProjectsList = (): JSX.Element => {
 
   return (
     <Row as="ul" className="list-unstyled">
-      {projects.map((project: Project) => (
-        <Col as="li" xs={3} sm={4} key={project.id}>
-          <ProjectCard project={project} />
+      {projects.map((project: Project, i: number) => (
+        <Col
+          as="li"
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          key={project.id}
+          className="container-card"
+        >
+          <ProjectCard project={project} backgroundColor={colors[i]} />
         </Col>
       ))}
     </Row>
