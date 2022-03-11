9b521836112101634247e79ae798384751de2882
Mario González <mario@mariogl.com>
2022-03-11 07:19:52 +0100

    Added last open PR to project card

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,11 +2,12 @@ import { rest } from "msw";
 import { randomProjects } from "./projects";
 
 export const handlers = [
-  rest.get(
-    `${process.env.REACT_APP_GITHUB_API}repos/isdi-coders-2022/*`,
-    (req, res, ctx) =>
-      res(ctx.status(200), ctx.json({ pushed_at: "2022-03-10T09:44:15Z" }))
-  ),
+  rest.get("https://api.github.com/repos/*", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([{ commit: { author: { date: "2022-03-10T09:44:15Z" } } }])
+    );
+  }),
   rest.get(process.env.REACT_APP_API_URL, (req, res, ctx) =>
     res(
       ctx.status(200),
