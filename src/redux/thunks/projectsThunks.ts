991ea57121bb91415f36e9227d15da38e8a14d36
Mario González <mario@mariogl.com>
2022-03-12 08:39:16 +0100

    Added SonarQube data to project card

--- src/redux/thunks/projectsThunks.ts
+++ src/redux/thunks/projectsThunks.ts
@@ -9,8 +9,7 @@ export const loadProjectsThunk =
     const apiUrl = process.env.REACT_APP_API_URL as string;
     const { data } = await axios.get<{ projects: Project[] }>(apiUrl, {
       headers: {
-        authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMjQ2ZTQzYThlZDBmY2FmZDE5NDZmYiIsIm5hbWUiOiJNYXJpbyIsImlhdCI6MTY0Njg1MzQ3NCwiZXhwIjoxNjQ3MDI2Mjc0fQ.ZMSi38Zx7x5GpsmLsrfDZepsfMP-Hy9hfK9dNRNn1gU",
+        authorization: `Bearer ${process.env.REACT_APP_TEMPORARY_JWT}`,
       },
     });
     dispatch(loadProjectsAction(data.projects));
